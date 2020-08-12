d20ee8dbccf57cdba3040dc505aa3e8c199c63b0
Phodal Huang <h@phodal.com>
2020-08-12 09:24:34 +0800

    refactor: coment unsued code

--- plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/plugin/AppPlugin.kt
+++ plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/plugin/AppPlugin.kt
@@ -33,17 +33,16 @@ abstract class AppPlugin : Plugin<Project> {
     }
 
     override fun apply(project: Project) {
-        // Add the 'template' extension object
-        val extension = project.extensions.create(EXTENSION_NAME, TemplateExtension::class.java, project)
+//        // Add the 'template' extension object
+//        val extension = project.extensions.create(EXTENSION_NAME, TemplateExtension::class.java, project)
+//        // Add a task that uses configuration from the extension object
+//        project.tasks.register(TASK_NAME, TemplateExampleTask::class.java) {
+//            it.tag.set(extension.tag)
+//            it.message.set(extension.message)
+//            it.outputFile.set(extension.outputFile)
+//        }
 
         this.project = project
-
-        // Add a task that uses configuration from the extension object
-        project.tasks.register(TASK_NAME, TemplateExampleTask::class.java) {
-            it.tag.set(extension.tag)
-            it.message.set(extension.message)
-            it.outputFile.set(extension.outputFile)
-        }
 
         configureProject()
 
