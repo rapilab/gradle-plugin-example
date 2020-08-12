d3f48a2910396ab8bec459a016e157fc02402059
Phodal Huang <h@phodal.com>
2020-08-12 09:25:25 +0800

    build: change ci jobs

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
     id("com.phodal.gradroid.plugin")
 }
 
-phodalConfig {
-    message.set("Just trying this gradle plugin...")
-}
+//phodalConfig {
+//    message.set("Just trying this gradle plugin...")
+//}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
