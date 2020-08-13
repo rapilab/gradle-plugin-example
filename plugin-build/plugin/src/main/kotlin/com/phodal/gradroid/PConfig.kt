2e0f94fd642f23a8f3f0a35e88c6c19673a3e7d5
Phodal Huang <h@phodal.com>
2020-08-13 10:34:25 +0800

    feat: try to set config for extension

--- plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/PConfig.kt
+++ plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/PConfig.kt
@@ -2,23 +2,16 @@ package com.phodal.gradroid
 
 import org.gradle.api.Named
 import org.gradle.api.tasks.Input
-import java.io.Serializable
 
-class PConfig(private val name: String)  {
+class PConfig(private val name: String): Named {
     private lateinit var mMessage: String
 
-    fun getName(): String {
+    override fun getName(): String {
         return this.name
     }
 
     @Input
-    fun setMessage(message: String): PConfig {
-        mMessage = message
-        return this
-    }
-
-    @Input
-    fun getMessage(): String {
-        return mMessage
+    fun message(msg: String) {
+        this.mMessage = msg
     }
 }
--- plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/plugin/AppExtension.kt
+++ plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/plugin/AppExtension.kt
@@ -1,14 +1,15 @@
 package com.phodal.gradroid.plugin
 
 import com.phodal.gradroid.PConfig
+import org.gradle.api.Action
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.internal.project.ProjectInternal
 import org.gradle.internal.reflect.Instantiator
 
 open class AppExtension(project: ProjectInternal, var instantiator: Instantiator) : BaseExtension(project, instantiator) {
-    var pConfig:  NamedDomainObjectContainer<PConfig>
+    var pConfig: NamedDomainObjectContainer<PConfig> = project.container(PConfig::class.java, PConfigFactory(instantiator))
 
-    init {
-        this.pConfig = project.container(PConfig::class.java, PConfigFactory(instantiator))
+    fun pConfig(action: Action<in NamedDomainObjectContainer<PConfig>>) {
+        action.execute(pConfig);
     }
 }
