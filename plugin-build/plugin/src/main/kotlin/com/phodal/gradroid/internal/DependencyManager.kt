c6c2ff062136bce21f463c05e0a1d560c1245db1
Phodal Huang <h@phodal.com>
2020-08-15 09:13:03 +0800

    refactor: remove unused code

--- plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/internal/DependencyManager.kt
+++ plugin-build/plugin/src/main/kotlin/com/phodal/gradroid/internal/DependencyManager.kt
@@ -8,33 +8,14 @@ import org.gradle.internal.impldep.org.bouncycastle.asn1.x500.style.RFC4519Style
 
 class DependencyManager(val project: Project, val extraModelInfo: ExtraModelInfo) {
     fun resolveDependencyForConfig(deps: VariantDependencies) {
-        var compile: Configuration  = project.configurations.maybeCreate("_${name}Compile")
+        val compile: Configuration  = project.configurations.maybeCreate("_${name}Compile")
 
         val compileConfigs = hashSetOf<Configuration>()
-//        compileConfigs.add(provider.compileConfiguration)
         compile.setExtendsFrom(compileConfigs)
 
-//        var compileClasspath: Configuration = deps.compileConfiguration
-        var compileClasspath: Configuration = compile
-//        var packageClasspath: Configuration = deps.packageConfiguration
+        val compileClasspath: Configuration = compile
 
-
-        // TODO - defer downloading until required -- This is hard to do as we need the info to build the variant config.
         val artifacts: Map<ModuleVersionIdentifier, List<ResolvedArtifact>> = hashMapOf()
-
-        collectArtifacts(compileClasspath, artifacts)
-        println(compileClasspath.allDependencies.buildDependencies)
-        println(compileClasspath.incoming.resolutionResult.root.dependencies)
-//        collectArtifacts(packageClasspath, artifacts)
-
-
-
-        // --- Handle the external/module dependencies ---
-        // keep a map of modules already processed so that we don't go through sections of the
-        // graph that have been seen elsewhere.
-//        val foundLibraries: Map<ModuleVersionIdentifier, List<LibInfo>> = hashMapOf()
-//        val foundJars: Map<ModuleVersionIdentifier, List<JarInfo>> = hashMapOf()
-
     }
 
     private fun collectArtifacts(
