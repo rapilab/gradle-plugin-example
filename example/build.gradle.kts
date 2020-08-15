74905d1ec8d56d798eed15b41714f7dc41ff30bd
Phodal Huang <h@phodal.com>
2020-08-15 11:55:08 +0800

    feat: try to save gradle for testing

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -4,13 +4,6 @@ plugins {
     id("com.android.application")
 }
 
-allprojects {
-    repositories {
-        google()
-        jcenter()
-    }
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
