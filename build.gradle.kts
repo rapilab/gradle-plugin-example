272c2810340456d65dc02c055f100cf6859bb442
Phodal Huang <h@phodal.com>
2020-08-15 11:50:56 +0800

    test: try found android impl

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,17 @@ plugins {
     id("com.github.ben-manes.versions") version BuildPluginsVersion.VERSIONS_PLUGIN
 }
 
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath(kotlin("gradle-plugin", version = "1.3.72"))
+    }
+}
+
 allprojects {
     repositories {
         google()
--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,27 +1,32 @@
 plugins {
-    java
-    id("com.phodal.gradroid.plugin")
+//    java
+//    id("com.phodal.gradroid.plugin")
+    id("com.android.application")
 }
 
-//phodalConfig {
-//    message.set("Just trying this gradle plugin...")
-//}
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-phodal {
-    pConfig {
-        applicationId = "com.phodal.gra"
-        message("phodal")
-        sdkVersion(12)
-    }
-}
+//phodal {
+//    pConfig {
+//        applicationId = "com.phodal.gra"
+//        message("phodal")
+//        sdkVersion(12)
+//    }
+//}
 
 dependencies {
-//    implementation(kotlin("stdlib-jdk7"))
 //    implementation(gradleApi())
     implementation("net.sf.proguard:proguard-gradle:5.2.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+//    phodalImpl()
 }
