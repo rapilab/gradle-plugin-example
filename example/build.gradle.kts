361a2a1a693f6dbc21b4755e70622961cb662d4f
Phodal Huang <h@phodal.com>
2020-08-16 10:08:16 +0800

    fix: fix impl issue

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-//    java
-//    id("com.phodal.gradroid.plugin")
-    id("com.android.application")
+    java
+    id("com.phodal.gradroid.plugin")
+//    id("com.android.application")
 }
 
 java {
@@ -9,17 +9,17 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-//phodal {
-//    pConfig {
-//        applicationId = "com.phodal.gra"
-//        message("phodal")
-//        sdkVersion(12)
-//    }
-//}
+phodal {
+    pConfig {
+        applicationId = "com.phodal.gra"
+        message("phodal")
+        sdkVersion(12)
+    }
+}
 
 dependencies {
 //    implementation(gradleApi())
     implementation("net.sf.proguard:proguard-gradle:5.2.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.1")
 //    phodalImpl()
 }
